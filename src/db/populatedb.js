6cd25ffb5ac4d430f7ea831ba763dab4281c4121
olopok <pabisa.pb@gmail.com>
2026-09-07 20:29:32 +0100

    populate the db

--- src/db/populatedb.js
+++ src/db/populatedb.js
@@ -4,22 +4,29 @@ dotenv.config();
 const { Client } = require("pg");
 
 const SQL = `CREATE TABLE IF NOT EXISTS categories (
-id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-name VARCHAR(100) NOT NULL);
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(100) NOT NULL);
 
-CREATE TABLE products (
+CREATE TABLE IF NOT EXISTS products (
     id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(200) NOT NULL,
-    descrizione TEXT
+    quantity INT,
+    category_id INT,
+    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL
 );
 
-CREATE TABLE products_categories (
-    product_id INT NOT NULL,
-    category_id INT NOT NULL,
-    PRIMARY KEY (product_id, category_id),
-    FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE
-);
+INSERT INTO categories (name) VALUES
+  ('paints'), ('tools'), ('fillers');
+
+INSERT INTO products (name, quantity, category_id) VALUES
+  ('Vinyl paint', 5, 1),
+  ('Acrylic paint', 3, 1),
+  ('Wood stain', 2, 1),
+  ('Brush', 12, 2),
+  ('Roller', 5, 2),
+  ('Scraper', 1, 2),
+  ('Ready mixed', 3, 3),
+  ('Wood filler', 1, 3);
 `;
 
 async function main() {
