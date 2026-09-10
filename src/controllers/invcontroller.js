92ff24f4c7ea36f57a941fbc23db179d09cf62bf
olopok <pabisa.pb@gmail.com>
2026-09-10 19:45:34 +0100

    Added the items table to the UI

--- src/controllers/invcontroller.js
+++ src/controllers/invcontroller.js
@@ -2,17 +2,18 @@ const { body, validationResult } = require("express-validator");
 const db = require("../db/queries");
 
 function homeRender(req, res) {
-  res.render("index", { titleh1: "Shop Inventory", categories: [] });
+  res.render("index", { categories: [], items: [] });
 }
 
 async function getCategories(req, res) {
   const categories = await db.getAllCategories();
-  res.render("index", { titleh1: "Shop Inventory", categories: categories });
+  res.render("index", { categories: categories, items: [] });
 }
 
 async function getCategoryItems(req, res) {
   const { id } = req.params;
   const categoryItems = await db.getAllCategoryItems(id);
-  console.log("items", categoryItems);
+  const categories = await db.getAllCategories();
+  res.render("index", { items: categoryItems, categories: categories });
 }
 module.exports = { homeRender, getCategories, getCategoryItems };
--- src/db/queries.js
+++ src/db/queries.js
@@ -4,7 +4,6 @@ async function getAllCategories() {
   const { rows } = await pool.query(
     "SELECT c.id, c.name, COUNT(p.id) AS items_quantity FROM categories c LEFT JOIN products p ON p.category_id = c.id GROUP BY c.id, c.name ORDER BY items_quantity DESC",
   );
-  console.log(rows);
   return rows;
 }
 
